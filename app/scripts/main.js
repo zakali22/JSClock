d46dd89a26b6dd75af8c9ee25839a3287081be75
Zakeria Ali <zak.fined@gmail.com>
2017-07-09 20:19:04 +0100

    Add the functionality for the seconds and minutes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+  var secondsHand = document.querySelector('.sec-hand');
+  var minHand = document.querySelector('.min-hand');
+
+  function getTime() {
+    var time = new Date();
+    var secs = time.getSeconds();
+    var min = time.getMinutes();
+    var secondsDegree = ((secs / 60) * 360 + 90);
+    var minDegree = ((secs / 3600) * 360 + 90);
+    secondsHand.style.transform = `rotate(${secondsDegree}deg)`;
+    minDegree.style.transform = `rotate(${minDegree}deg)`;
+  }
+
+  setInterval(getTime, 1000);
