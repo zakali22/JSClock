eedfe09ee8ef4f1c18f404e60882139c4a1b1889
Zakeria Ali <zak.fined@gmail.com>
2017-07-09 20:52:57 +0100

    Add functionality to the hour hand

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,22 @@
   var secondsHand = document.querySelector('.sec-hand');
   var minHand = document.querySelector('.min-hand');
-
+  var hourHand = document.querySelector('.hour-hand');
   function getTime() {
     var time = new Date();
+
     var secs = time.getSeconds();
     var min = time.getMinutes();
-    var secondsDegree = ((secs / 60) * 360 + 90);
-    var minDegree = ((min / 3600) * 360 + 90);
+    var hour = time.getHours();
+
+    var secondsDegree = ((secs / 60) * 360) + 90;
+    var minDegree = ((min / 60) * 360) + ((secs / 60) * 6) + 90;
+    var hourDegree = ((hour / 12) * 360) + ((min / 60 ) * 30) + 90;
+
     secondsHand.style.transform = `rotate(${secondsDegree}deg)`;
     minHand.style.transform = `rotate(${minDegree}deg)`;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
   }
 
   setInterval(getTime, 1000);
+
+  getTime();
